f1053a7ded17bb3d207c5cfc130ed6265c855698
maxim <borisov130490@gmail.com>
2024-11-04 19:26:04 +0300

    f1

--- src/db/get_cases.py
+++ src/db/get_cases.py
@@ -8,6 +8,8 @@ def get_all_tasks():
     return tasks
 
 def get_tasks_by_time(current_time: datetime):
-    adjusted_time = current_time - timedelta(hours=3)
+    adjusted_time = (current_time
+                    #   - timedelta(hours=3)
+                     )
     tasks = Case.select().where(Case.date == adjusted_time)
     return tasks
